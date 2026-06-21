26f7d6c400fb552fea86e262f5a02105517cf523
Mayura Jayasinghe <88314486+mayura0001@users.noreply.github.com>
2026-06-21 09:48:29 +0530

    simple list memory

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 print("successfully loaded env variables")
 
+chats = []
+
 def send(user_input):
     response = requests.post(
         "https://openrouter.ai/api/v1/chat/completions",
@@ -28,7 +30,17 @@ def send(user_input):
         return {"response": f"Error {response.status_code}", "reasoning": ""}
 
     msg = response.json()["choices"][0]["message"]
+    
+    mydict = {}
+    mydict["input"] = user_input
+    mydict["reasoning"] = msg.get("reasoning")
+    mydict["content"] = msg.get("content")
+    chats.append(mydict)
+
     return {
         "response": msg.get("content", ""),
         "reasoning": msg.get("reasoning", "")
     }
+
+send("What is the capital of France?")
+print(chats)
